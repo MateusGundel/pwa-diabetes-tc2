71cb895be668e3683643d664787e4f611f45d4b2
Mateus Gündel <mateus8923@gmail.com>
2022-09-26 01:22:48 -0300

    Alterada tela de login e adicionado bootstrap icons ao projeto

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,47 @@
 <template>
-    <div class="container">
-        <div class="login">
-            <label for="user">Email</label>
-            <input name="user" class="user" type="text">
-            <label for="password">Senha</label>
-            <input name="password" class="password" type="password">
-            <button>Login</button>
+  <div class="container vertical-center">
+    <form class="row">
+      <div class="col-12">
+        <div class="input-group mb-3">
+          <span class="input-group-text"><i class="bi bi-person-fill"></i></span>
+          <input type="text" class="form-control" placeholder="Email" aria-label="Email"
+                 aria-describedby="basic-addon1">
         </div>
-    </div>
+      </div>
+      <div class="col-12">
+        <div class="input-group mb-3">
+          <span class="input-group-text"><i class="bi bi-key-fill"></i></span>
+          <input type="text" class="form-control" placeholder="Senha" aria-label="Senha">
+        </div>
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-primary button-login">Login</button>
+      </div>
+    </form>
+  </div>
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
 export default defineComponent({
-    name: 'LoginApp',
+  name: 'LoginApp',
 });
 </script>
 
-<style src="../assets/styles/login.css">
+<style scoped>
 
+.button-login {
+  background-color: #0B4F6C;
+  border-color: #0B4F6C;
+}
+
+.vertical-center {
+  min-height: 90%; /* Fallback for browsers do NOT support vh unit */
+  min-height: 90vh; /* These two lines are counted as one :-)       */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+
 
 createApp(App).use(router).mount('#app')
 
