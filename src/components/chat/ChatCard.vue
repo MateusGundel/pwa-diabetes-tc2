e1e394483f0d665c9268a30abee8763912e200ba
Mateus Gündel <mateus8923@gmail.com>
2022-11-15 19:19:31 -0300

    Adicionada imagens no chat com a doris

--- src/components/chat/ChatCard.vue
+++ src/components/chat/ChatCard.vue
@@ -3,6 +3,10 @@
     <chat-image :type="chat_object.type"></chat-image>
     <chat-text :type="chat_object.type" :message="chat_object.message"></chat-text>
   </div>
+  <div v-else-if="chat_object.type==='doris-image'" class="d-flex flex-row justify-content-start mb-4">
+    <chat-image type="doris"></chat-image>
+    <img class="chat-image" :src="chat_object.message">
+  </div>
   <div v-else class="d-flex flex-row justify-content-end mb-4">
     <chat-text :type="chat_object.type" :message="chat_object.message"></chat-text>
     <chat-image :type="chat_object.type"></chat-image>
@@ -46,5 +50,19 @@ export default {
   background-color: #abd3f3;
   border-color: #abd3f3;
   color: black;
+}
+
+@media (max-width: 600px) {
+  .chat-image {
+    max-width: 90%;
+    padding: 1rem 1rem 1rem 1rem;
+  }
+}
+
+@media (min-width: 600px) {
+  .chat-image {
+    max-width: 50%;
+    padding: 1rem 1rem 1rem 1rem;
+  }
 }
 </style>
